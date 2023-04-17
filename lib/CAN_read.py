a7a72d4319380061813176c3dd091634bfd5d3b0
NielGi <122493510+NielGi@users.noreply.github.com>
2023-04-17 11:41:39 -0700

    CAN bus logger

---
+++ lib/CAN_read.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append()
+import can
+import cantools
+import lib.CAN_read as CAN_read
+
+def CAN_INIT():
+    os.system('sudo ip link set can0 type can bitrate 500000')
+    os.system('sudo ifconfig can0 up')
+
+    can_interface = 'socketcan'
+    can_channel   = 'can0'
+    can_bus       = can.interface.Bus(channel=can_channel, interface=can_interface)
+    return can_bus
+
+def read_message(can_bus):
+    message = can_bus.recv()
+    return message
